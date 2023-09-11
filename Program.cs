ebbd7374442e94cc52249c8f8a28f68a67695dbc
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-12 00:10:41 +0100

    Readme and Code

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program {
+    public static void Main(string[] args) {
+       int MyNumber = 25;
+       int NumberResult = Square(MyNumber);
+    }
+
+    int Square(int number) {
+        return number * number;
+    }
+
+}
